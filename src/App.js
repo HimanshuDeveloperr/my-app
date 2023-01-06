6c7867ae021a41a2059e2e483dd2efcde4a63d99
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-07 00:41:58 +0530

    concept of props and how we can re use compusingit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-
-
 import ExpenseItem from "./components/ExpenseItem";
 
-
-
-
 function App() {
-    return (
-      <div>
-        <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
-      </div>
-    );
-  }
-  
-  export default App;
-  
-  //we can use an html element in the js file because of the feature called jsx.
-  // abd because of the all the transformation that is being handled by the react behind the scenes before giving it to the browser
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location:"market"
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), location:"mall" },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location:"insurance company"
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location:"store"
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        location={expenses[0].location}
+      ></ExpenseItem>
 
-  //always remember lowercase elements are built in html elements and uppercase are custom html elements .
+<ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        location={expenses[1].location}
+
+      ></ExpenseItem>
+
+<ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        location={expenses[2].location}
+
+      ></ExpenseItem>
+
+<ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        location={expenses[3].location}
+
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
+
+//we can use an html element in the js file because of the feature called jsx.
+// abd because of the all the transformation that is being handled by the react behind the scenes before giving it to the browser
+
+//always remember lowercase elements are built in html elements and uppercase are custom html elements .
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,21 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const expenseDate=new Date(2023, 1, 6)
-    const foodItem='banana';
-    const cost=10;
-    const LocationOfExpenditure='market'
+function ExpenseItem(props){
+   
+    
 
     return(
 
 
           
         <div className="expense-item">
-       <div>{expenseDate.toDateString()}</div> 
+       <div>{props.date.toDateString()}</div> 
        {/* it returns us the date as a string */}
     <div className="expense-item__description">
-        <h2>{foodItem}</h2>
-        <h2>{LocationOfExpenditure}</h2>
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
         <div className="expense-item__price">
-            rs{cost}
+            rs{props.amount}
         </div>
     </div>
     
