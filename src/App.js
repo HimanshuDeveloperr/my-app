7e5cac3f757ebb595bf011e1421e2aa1c981f284
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-06 22:35:54 +0530

    CreatedAndImportedAComponentInApp.JsFILE

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
+
+import ExpenseItem from "./components/ExpenseItem";
+
+
+
+
 function App() {
     return (
       <div>
         <h2>Let's get started!</h2>
-        <p>this is also visible</p>
+        <ExpenseItem></ExpenseItem>
       </div>
     );
   }
@@ -11,3 +18,5 @@ function App() {
   
   //we can use an html element in the js file because of the feature called jsx.
   // abd because of the all the transformation that is being handled by the react behind the scenes before giving it to the browser
+
+  //always remember lowercase elements are built in html elements and uppercase are custom html elements .
