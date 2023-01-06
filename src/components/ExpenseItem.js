b6c3ca0b7f999f2c22c2c246c6d1ef846e70546e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-06 22:42:47 +0530

    first task

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,20 @@
 function ExpenseItem(){
 
-    return <h2>Expense Item!</h2>
+    return(
+
+    <div>
+    <h2>Expense Item!
+
+    </h2>
+
+    <h6>Food Rs 10</h6>
+    <h6>Petrol Rs 100</h6>
+    <h6>Movies Rs 200</h6>
+
+    </div>
+
+    )
+            
 }
 
 
