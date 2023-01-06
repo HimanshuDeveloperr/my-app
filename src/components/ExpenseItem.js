8c938bcd102fc4152fe4ca9365c9b7f382040517
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-07 00:00:42 +0530

    assigning variables in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const expenseDate=new Date(2023, 1, 6)
+    const foodItem='banana';
+    const cost=10;
+    const LocationOfExpenditure='market'
 
     return(
+
+
           
         <div className="expense-item">
-       <div>Expense Item</div>
+       <div>{expenseDate.toDateString()}</div> 
+       {/* it returns us the date as a string */}
     <div className="expense-item__description">
-        <h2>food</h2>
+        <h2>{foodItem}</h2>
+        <h2>{LocationOfExpenditure}</h2>
         <div className="expense-item__price">
-            rs 10
+            rs{cost}
         </div>
     </div>
     
