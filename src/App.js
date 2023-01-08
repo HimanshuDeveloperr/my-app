546526cc8709841e1121ee3bd2ea0ccf7f8cf64e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-08 15:18:56 +0530

    CreatedExpenseDetailsComponents IMP

--- src/App.js
+++ src/App.js
@@ -7,40 +7,42 @@ function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      location:"market"
+      location: "market",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), location:"mall" },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "mall",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      location:"insurance company"
+      location: "insurance company",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      location:"store"
+      location: "store",
     },
-    
   ];
-
-  
 
   return (
     <div>
       <h2>Let's get started!</h2>
       {/* loope through the expensex array of objects using map method inside the {curly braces} and change the {} after arrow to () as because of this return takes palce important note.and then created the custom html with the required attributes */}
-      {expenses.map((e)=>(
-         
-      <ExpenseItem
-        title={e.title}
-        amount={e.amount}
-        date={e.date}
-        location={e.location}
-      ></ExpenseItem>
+      {expenses.map((e) => (
+        <ExpenseItem
+          title={e.title}
+          amount={e.amount}
+          date={e.date}
+          location={e.location}
+        ></ExpenseItem>
       ))}
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,34 +1,16 @@
+import ExpenseDetails from "./ExpenseDetails";
 
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem(props){
-   
-    
-    
-
-    return(
-
-
-          
-        <div className="expense-item">
-       <ExpenseDate date={props.date}></ExpenseDate>
-       
-    <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">
-            rs{props.amount}
-        </div>
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetails amount={props.amount} />
     </div>
-    
-   </div>
-   
-
-    )
-            
+  );
 }
-
 
 export default ExpenseItem;
