9a5da2c65644337063310a82af9ac795e98c58c6
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-09 00:41:59 +0530

    creatingManagableFolders

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,8 +5,7 @@ import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
-import Card from "./Card";
-
+import Card from '../UI/Card';
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
