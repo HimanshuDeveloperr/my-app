59741e0fd35460c4b45b7f748ca875c2982f8bef
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-10 15:41:09 +0530

    addedEventListeners&DeleteButton

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,10 +7,26 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from '../UI/Card';
 function ExpenseItem(props) {
+  const clickHandler=()=>{
+    console.log(props.id)
+
+
+    const ele = document.getElementById(props.id)
+    // while(ele.length > 0){
+      ele.parentNode.removeChild(ele)
+    // }
+   
+
+    
+  }
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.id}>
+      
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails amount={props.amount} />
+
+    
+      <button onClick={clickHandler}>delete</button>
     </Card>
   );
 }
