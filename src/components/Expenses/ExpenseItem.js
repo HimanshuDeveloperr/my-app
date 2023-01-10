1a991f2a015e8e36229c4c38fd25a3315fa509c9
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-10 18:57:24 +0530

    addedAnewButtonWithEventAndStateOnIt

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,33 +1,32 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import ExpenseDetails from "./ExpenseDetails";
 
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 function ExpenseItem(props) {
+  const [amount, addedAmount] = useState(props.amount);
+  const add = () => {
+    addedAmount("100");
+  };
+  const [title, setTitle] = useState(props.title);
 
-  const [title,setTitle]=useState(props.title);  
-  // using array destructing as the usestate will return the array with one as var and one as updted var that we want to update.second element is a function we could call to update the variable.
-  const clickHandlerr=()=>{
-     setTitle('calling settitle')
-  }
+  const clickHandlerr = () => {
+    setTitle("calling settitle");
+  };
 
   return (
     <Card className="expense-item" id={props.id}>
-      
       <ExpenseDate date={props.date}></ExpenseDate>
       <h2>{title}</h2>
 
-      <ExpenseDetails amount={props.amount} />
+      <ExpenseDetails amount={amount} />
 
-    
-      
+      <button onClick={add}>+</button>
+
       <button onClick={clickHandlerr}>change title</button>
-
-      
-      
     </Card>
   );
 }
