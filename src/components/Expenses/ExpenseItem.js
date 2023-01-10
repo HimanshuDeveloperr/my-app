5cda588c623cb54988188ca66850a665af8d2ef3
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-10 17:30:56 +0530

    updateVarUsingUseState & create ChangeTittleButton

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import ExpenseDetails from "./ExpenseDetails";
 
@@ -7,28 +7,27 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from '../UI/Card';
 function ExpenseItem(props) {
-  const clickHandler=()=>{
-    console.log(props.id)
 
-
-    const ele = document.getElementById(props.id)
-    
-      
-      console.log(ele.parentNode)
-      ele.parentNode.removeChild(ele)
-    
-   
-
-    
+  const [title,setTitle]=useState(props.title);  
+  // using array destructing as the usestate will return the array with one as var and one as updted var that we want to update.second element is a function we could call to update the variable.
+  const clickHandlerr=()=>{
+     setTitle('calling settitle')
   }
+
   return (
     <Card className="expense-item" id={props.id}>
       
       <ExpenseDate date={props.date}></ExpenseDate>
+      <h2>{title}</h2>
+
       <ExpenseDetails amount={props.amount} />
 
     
-      <button onClick={clickHandler}>delete</button>
+      
+      <button onClick={clickHandlerr}>change title</button>
+
+      
+      
     </Card>
   );
 }
