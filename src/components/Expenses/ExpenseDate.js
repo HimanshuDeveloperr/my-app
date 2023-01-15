33b1601c326db0fcdad54f16daf0dab93b509a6e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-15 14:46:30 +0530

    lifting the state up

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,4 @@
-//for rendering the date of the expense.
 
-//components in react are just regural function returning the jsx code.
 import React from "react";
 
 import "./ExpenseDate.css";
--- src/index.js
+++ src/index.js
@@ -7,13 +7,4 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-//here create root is a method in react library used to create main entry point the main hook of the ui that we build using react.
-// as we are inserting our react all functionalities in this div with the id of root in it we basically starting injecting the react methods from here only
 
-//reactdom=library
-//createroot=reactdom method
-//render is used to tell the react what should it render in that root const created by createRoot.
-
-//app here is our first component .
-
-// The src/ directory contains the React JavaScript code for your project. Most of the work you do will be in that directory.
