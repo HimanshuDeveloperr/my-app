de3f1c0cca9d0ca92ff3b0ca806017c169c5a715
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-15 20:18:45 +0530

    RenderingListsOfData ThroughPropsAndMapMethod IMP

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,46 +1,32 @@
+import React, { useState } from "react";
 
-import React,{useState} from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter';
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-const Expenses=(props)=> {
-  const [filteredYear, setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        id={props.items[0].id}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        id={props.items[1].id}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        id={props.items[2].id}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        id={props.items[3].id}
-      />
+      {props.items.map((expenses) => (
+        <ExpenseItem
+          title={expenses.title}
+          amount={expenses.amount}
+          date={expenses.date}
+          id={expenses.id}
+        />
+      ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
