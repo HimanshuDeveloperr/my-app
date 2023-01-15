f6ca83e827b48d678b68d79268d4a2875acd082f
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-15 15:42:07 +0530

    AddedFilterComponent

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,11 +8,7 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
   
-  // const [title, setTitle] = useState(props.title);
-
-  // const clickHandler = () => {
-  //   setTitle("calling settitle");
-  // };
+  
 
   return (
     <Card className='expense-item'>
@@ -21,7 +17,6 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
   
