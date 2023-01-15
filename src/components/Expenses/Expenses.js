af28af2635f3aa82b2cd93ba03bde90ae66a2264
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-15 21:37:23 +0530

    AddedFilterFunctionallity REALLY IMP

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,14 +10,18 @@ const Expenses = (props) => {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+
   };
+  const filteredExpnses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expenses) => (
+      {filteredExpnses.map((expenses) => (
         <ExpenseItem
         key={expenses.id}
           title={expenses.title}
@@ -30,13 +34,3 @@ const Expenses = (props) => {
 };
 
 export default Expenses;
-
-//-when we try to add anything without having keys. React renders this new item as the last item in that list of div and update all the items and replace their content such it again matches the order of the items in my array.Final result is correct here But from performance perspective it is not that great and it also leads to some bugs if expense items would be a state full item
-
-
-
-//it helps react to tell and placed the updation or any changes we made as to where it should be placed and in which order it is like a identifier
-
-
-
-//we should not use index as keys because it is not directly attached to the content of the item and can be same for the given item.
