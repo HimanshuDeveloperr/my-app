2958f15cfc72cd8674cce8276a08bc341eb0ff78
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-16 12:26:35 +0530

    FinalWay conditionalRendering MOST IMP

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,23 +14,26 @@ const Expenses = (props) => {
   const filteredExpnses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expenseContent=<p>No Expense Is Found</p>
+
+  if(filteredExpnses.length >0){
+    expenseContent=filteredExpnses.map((expenses) => (
+      <ExpenseItem
+        key={expenses.id}
+        title={expenses.title}
+        amount={expenses.amount}
+        date={expenses.date}
+      />
+    ))
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpnses.length === 0 && <p>No Expense Is Found</p>}
-      {/* after the and operator anything that we give will be renderted and return if the before and statement is true */}
-      {filteredExpnses.length > 0 &&
-        filteredExpnses.map((expenses) => (
-          <ExpenseItem
-            key={expenses.id}
-            title={expenses.title}
-            amount={expenses.amount}
-            date={expenses.date}
-          />
-        ))}
+     {expenseContent}
     </Card>
   );
 };
